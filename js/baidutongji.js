dc8775f77b4ca4b92d5c309c42d1536460c9b454
Zach Chan <web_zach@foxmail.com>
2018-04-23 17:12:53 +0800

    增加百度统计

---
+++ js/baidutongji.js
@@ -0,0 +1,8 @@
+
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?9895e3fc97570f4aa8810b2c83ee065a";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
